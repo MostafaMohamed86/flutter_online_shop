133c484b64868c2d7bea1254691b0dfe72db0784
MostafaMohamed86 <mostafa.mohamed0869@gmail.com>
2023-04-29 04:59:16 +0300

    Creating ProductCard

--- lib/views/ui/mainscreen.dart
+++ lib/views/ui/mainscreen.dart
@@ -24,65 +24,60 @@ class MainScreen extends StatelessWidget {
     return Consumer<MainScreenNotifier>(
       builder: (context, mainScreenNotifier, child) {
         return Scaffold(
-        backgroundColor: const Color(0xFFE2E2E2),
-        body: pageList[mainScreenNotifier.pageIndex],
-        bottomNavigationBar: SafeArea(
-            child: Padding(
-          padding: EdgeInsets.all(8),
-          child: Container(
-            padding: EdgeInsets.all(12),
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.all(Radius.circular(16))),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                BottomNavWidget(
-                  onTap: () {
-                    mainScreenNotifier.pageIndex = 0;
-                  },
-                  icon: mainScreenNotifier.pageIndex == 0
-                  ? MaterialCommunityIcons.home
-                  : MaterialCommunityIcons.home_outline
-                ),
-                BottomNavWidget(
-                  onTap: () {
-                    mainScreenNotifier.pageIndex = 1;
-                  },
-                  icon: mainScreenNotifier.pageIndex == 1
-                  ? Ionicons.search
-                  : Ionicons.search_circle_outline
-                ),
-                BottomNavWidget(
-                  onTap: () {
-                    mainScreenNotifier.pageIndex = 2;
-                  },
-                  icon: mainScreenNotifier.pageIndex == 2
-                  ? Ionicons.add
-                  : Ionicons.add_circle_outline
-                ),
-                BottomNavWidget(
-                  onTap: () {
-                    mainScreenNotifier.pageIndex = 3;
-                  },
-                  icon: mainScreenNotifier.pageIndex == 3
-                  ?Ionicons.cart
-                  :Ionicons.cart_outline
-                ),
-                BottomNavWidget(
-                  onTap: () {
-                    mainScreenNotifier.pageIndex = 4;
-                  },
-                  icon: mainScreenNotifier.pageIndex == 4
-                  ?Ionicons.person
-                  :Ionicons.person_outline
-                ),
-              ],
+          backgroundColor: const Color(0xFFE2E2E2),
+          body: pageList[mainScreenNotifier.pageIndex],
+          bottomNavigationBar: SafeArea(
+              child: Padding(
+            padding: EdgeInsets.all(8),
+            child: Container(
+              padding: EdgeInsets.all(12),
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.all(Radius.circular(16))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  BottomNavWidget(
+                      onTap: () {
+                        mainScreenNotifier.pageIndex = 0;
+                      },
+                      icon: mainScreenNotifier.pageIndex == 0
+                          ? MaterialCommunityIcons.home
+                          : MaterialCommunityIcons.home_outline),
+                  BottomNavWidget(
+                      onTap: () {
+                        mainScreenNotifier.pageIndex = 1;
+                      },
+                      icon: mainScreenNotifier.pageIndex == 1
+                          ? Ionicons.search
+                          : Ionicons.search_circle_outline),
+                  BottomNavWidget(
+                      onTap: () {
+                        mainScreenNotifier.pageIndex = 2;
+                      },
+                      icon: mainScreenNotifier.pageIndex == 2
+                          ? Ionicons.add
+                          : Ionicons.add_circle_outline),
+                  BottomNavWidget(
+                      onTap: () {
+                        mainScreenNotifier.pageIndex = 3;
+                      },
+                      icon: mainScreenNotifier.pageIndex == 3
+                          ? Ionicons.cart
+                          : Ionicons.cart_outline),
+                  BottomNavWidget(
+                      onTap: () {
+                        mainScreenNotifier.pageIndex = 4;
+                      },
+                      icon: mainScreenNotifier.pageIndex == 4
+                          ? Ionicons.person
+                          : Ionicons.person_outline),
+                ],
+              ),
             ),
-          ),
-        )),
-      );
+          )),
+        );
       },
     );
   }
